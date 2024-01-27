9e86e956a09b8d0287ff4b77d065bc5a36d43fc1
RohanRamidwar <rohanramidwar71@gmail.com>
2024-01-27 17:37:28 +0530

    backend

---
+++ backend/index.js
@@ -0,0 +1,8 @@
+//express server
+const express = require("express");
+const app = express();
+
+//start server
+app.listen(3000, () => {
+  console.log("Server is running Successfully at PORT 3000 !");
+});
