1105730226afa25b15c0e00778053de1702e01bd
RohanRamidwar <rohanramidwar71@gmail.com>
2024-02-03 01:37:45 +0530

    post controller added

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -2,20 +2,20 @@ const Post = require("../models/postModel");
 
 exports.createPost = async (req, res) => {
   try {
-    const { title, description } = req.body;
-    const post = await Post.create({ title, description });
+    const { title, body } = req.body;
+    const post = await Post.create({ title, body });
 
     res.status(200).json({
       success: true,
       data: post,
-      message: "Entry Created SUCCESSFULLY !",
+      message: "Post Created SUCCESSFULLY !",
     });
   } catch (err) {
     console.error(err);
     console.log(err);
     res.status(500).json({
       success: false,
-      data: "Server ERROR !",
+      data: "ERROR while creating post!",
       message: err.message,
     });
   }
@@ -23,19 +23,19 @@ exports.createPost = async (req, res) => {
 
 exports.getAllPosts = async (req, res) => {
   try {
-    const posts = await Post.find({});
+    const posts = await Post.find({}).populate("comments").exec();
 
     res.status(200).json({
       success: true,
       data: posts,
-      message: "FETCHED all posts Sucessfully !",
+      message: "Fetched all posts Sucessfully !",
     });
   } catch (err) {
     console.error(err);
     console.log(err);
     res.status(500).json({
       success: false,
-      data: "Server ERROR !",
+      data: "ERROR while fetching posts!",
       message: err.message,
     });
   }
